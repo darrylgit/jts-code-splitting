6e1f136afe78472d5fcafcc225928a9bddc7d2ee
darrylgit <darrylsmithup@gmail.com>
2020-05-05 19:56:00 -0400

    Implement rudimentary code splitting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Page1 from './components/page1';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      route: 'page1',
+      component: ''
+    };
+  }
+  onRouteChange = route => {
+    if (route === 'page1') {
+      this.setState({ route: route });
+    } else if (route === 'page2') {
+      import('./components/page2').then(Page2 => {
+        this.setState({ route: route, component: Page2.default });
+      });
+    } else if (route === 'page3') {
+      import('./components/page3').then(Page3 => {
+        this.setState({ route: route, component: Page3.default });
+      });
+    }
+  };
+
+  render() {
+    // if (this.state.route === 'page1') {
+    //   return <Page1 onRouteChange={this.onRouteChange} />;
+    // } else if (this.state.route === 'page2') {
+    //   return <Page2 onRouteChange={this.onRouteChange} />;
+    // } else {
+    //   return <Page3 onRouteChange={this.onRouteChange} />;
+    // }
+    if (this.state.route === 'page1') {
+      return <Page1 onRouteChange={this.onRouteChange} />;
+    } else {
+      return <this.state.component onRouteChange={this.onRouteChange} />;
+    }
+  }
 }
 
 export default App;
---
+++ src/components/page2.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+const Page2 = ({ onRouteChange }) => (
+  <div className='App'>
+    <header className='App-header'>
+      <img src={logo} className='App-logo' alt='logo' />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className='App-link'
+        href='https://reactjs.org'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Learn React
+      </a>
+    </header>
+    <button onClick={() => onRouteChange('page1')}>Page1</button>
+    <button className='disabled'>Page2</button>
+    <button onClick={() => onRouteChange('page3')}>Page3</button>
+  </div>
+);
+
+export default Page2;
